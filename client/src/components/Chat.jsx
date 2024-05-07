0e9a3c38e53f0b4f41a45ffb17da140c33d7960f
Aditya06kr <adityakr0679@gmail.com>
2024-05-07 18:24:36 +0530

    Some styling on files

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -184,45 +184,77 @@ const Chat = () => {
         {selectedUserId && (
           <>
             <div className="overflow-y-auto custom-scrollbar">
-              {UniqueMessages.map((msg) => (
-                <div className="w-full" key={msg._id} style={{ clear: "both" }}>
+              {UniqueMessages.map((msg) => {
+                if (msg.url) {
+                  var fileExtension = msg.url.split(".").pop();
+                  var isImage = ["jpg", "jpeg", "png", "gif", "bmp"].includes(
+                    fileExtension.toLowerCase()
+                  );
+                }
+                return (
                   <div
+                    className="w-full"
                     key={msg._id}
-                    className={
-                      "w-fit max-w-2xl p-1 px-3 m-2 rounded-2xl " +
-                      (userInfo.id === msg.sender
-                        ? "bg-pink-200 float-right"
-                        : "bg-pink-400 float-left")
-                    }
+                    style={{ clear: "both" }}
                   >
-                    {msg.text ? (
-                      <div>{msg.text}</div>
-                    ) : (
-                      <div className="">
-                        <a
-                          target="_blank"
-                          className="flex items-center gap-1 border-b"
-                          href={msg.url}
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="currentColor"
-                            className="w-4 h-4"
+                    <div
+                      key={msg._id}
+                      className={
+                        "w-fit max-w-2xl p-1 px-3 m-2 rounded-2xl " +
+                        (userInfo.id === msg.sender
+                          ? "bg-pink-200 float-right"
+                          : "bg-pink-400 float-left")
+                      }
+                    >
+                      {msg.text ? (
+                        <div>{msg.text}</div>
+                      ) : (
+                        <div className="flex items-center bg-pink-100 rounded-2xl p-1">
+                          <a
+                            href={msg.url}
+                            className="flex gap-1 items-center"
+                            target="_blank"
+                            rel="noopener noreferrer"
                           >
-                            <path
-                              fillRule="evenodd"
-                              d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                          {msg.name}
-                        </a>
-                      </div>
-                    )}
+                            {isImage ? (
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth="1.5"
+                                stroke="currentColor"
+                                className="w-7 h-7"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                                />
+                              </svg>
+                            ) : (
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="w-6 h-6"
+                              >
+                                <path
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                  d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+                                />
+                              </svg>
+                            )}
+                            <p className="font-semibold">{msg.name}</p>
+                          </a>
+                        </div>
+                      )}
+                    </div>
                   </div>
-                </div>
-              ))}
+                );
+              })}
               <div ref={messagesEndRef}></div>
             </div>
             <form
