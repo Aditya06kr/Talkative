9d127c5657411fcc9d3f5416ff7d1f9c6b8594bd
Aditya06kr <adityakr0679@gmail.com>
2024-05-11 02:15:37 +0530

    added new routes

--- api/routers/Chat.js
+++ api/routers/Chat.js
@@ -3,11 +3,17 @@ import getPeople from "../controllers/getPeople.js";
 import getMessages from "../controllers/getMessages.js";
 import uploadFile from "../controllers/uploadFile.js";
 import {upload} from "../middlewares/multer.js"
+import editMessage from "../controllers/editMessage.js";
+import deleteMessage from "../controllers/deleteMessage.js";
+import editFile from "../controllers/editFile.js";
 
 const router = Router();
 
 router.route("/messages/:userId").get(getMessages);
 router.route("/people").get(getPeople);
 router.post("/uploads",upload.single('file'),uploadFile);
+router.route("/editMessage/:editId").put(editMessage);
+router.route("/deleteMessage/:deleteId").delete(deleteMessage);
+router.route("/editFile/:editId").put(editFile);
 
 export default router;
