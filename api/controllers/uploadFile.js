b4df0e84b9c36bfe20eac7fd0e963eab14eda14b
Aditya06kr <adityakr0679@gmail.com>
2024-05-27 21:55:06 +0530

    destroyed initial file from cloudinary

--- api/controllers/uploadFile.js
+++ api/controllers/uploadFile.js
@@ -1,7 +1,23 @@
-import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import {
+  uploadOnCloudinary,
+  deleteFromCloudinary,
+} from "../utils/cloudinary.js";
 
 const uploadFile = async (req, res) => {
   const filePath = req.file?.path;
+  const prevFile = req.body.prevFile;
+
+  // Check whether the file exist or not and Delete if exist
+  if (prevFile!=="null") {
+    try {
+      const res = await deleteFromCloudinary(prevFile);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  // Upload on Cloudinary
   if (filePath) {
     const fileUploaded = await uploadOnCloudinary(filePath);
     res.json({ url: fileUploaded.url, name: fileUploaded.original_filename });
