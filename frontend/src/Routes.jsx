49db25ad00827e1b96ea7b8bd32d3c9fc76faf9d
Aditya06kr <adityakr0679@gmail.com>
2024-07-21 22:48:47 +0530

    Checking Server

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,16 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
 import RegisterOrLogin from "./components/RegisterOrLogin";
 import Chat from "./components/Chat";
 import { UserContext } from "./UserContext";
+import Loader from "./components/Loader.jsx";
 
 const Routes = () => {
-  const {userInfo} = useContext(UserContext);
-  const username = userInfo?.username;
-  if (username) {
-    return <Chat />;
-  } else {
-    return <RegisterOrLogin />;
+  const {userInfo}=useContext(UserContext);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkServerStatus = async () => {
+      try {
+        const res=await axios.get("/ping");
+        console.log(res.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Server is not ready yet, retrying...");
+        setTimeout(checkServerStatus, 5000);
+      }
+    };
+
+    checkServerStatus();
+  }, []);
+
+  if (loading) {
+    return <Loader />;
   }
+
+  const username = userInfo?.username;
+  return username ? <Chat /> : <RegisterOrLogin />;
 };
 
 export default Routes;
